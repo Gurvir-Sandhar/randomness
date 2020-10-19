9ef5f0b4054fe77957d45b6354c572ed228ef91b
Gurvir-Sandhar <gurvir2@pdx.edu>
2020-10-18 21:07:00 -0700

    messy but it works

--- hw2.c
+++ hw2.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #define MAXCHAR 1000
 
-void vigenere_cipher(){
 
+int vigenere_cipher(char* input, char* k){
+  int count = 0;
+  for(int i = 0; i < strlen(input); i++){
+    if(input[i] >= 97 && input[i] <= 122){
+      int temp = ( (input[i]-97) + k[i % strlen(k)] ) % 26 + 97;
+      input[i] = temp;
+      count++;
+    }
+  }
+  return count;
 }
 
 int main(){
@@ -37,13 +47,43 @@ int main(){
   printf("\n%s\n", input);
   printf("total letters: %f\n", total_letters);
 
+  //print letter frequencies
   for(int k = 0; k < 26; k++){
     char letter = k + 97;
     float freq = letters[k] / total_letters;
     printf("%c: %f\n", letter, freq);
   }
   
+  printf("\n%s\n", input);
+  char plaintext[MAXCHAR];
+  strcpy(plaintext, input);
+  //get key
+  char* k;
+ // int length = 0;
+ // while(length <= 0 || length > 4){
+    printf("\nType a key that is 1-4 char long...\n");
+    scanf("%s", k);
+ // length = strlen(key);
+ // printf("key length: %d", length);
+  
+  float count = vigenere_cipher(plaintext, k);
+
+  printf("\n%s\n", plaintext);
+  printf("cipher count %f\n", count);
+
+  for(int i = 0; i < 26; i++){
+    letters[i] = 0;
+  }
+
+  for(int i = 0; i < count; i++){
+    letters[plaintext[i]-97]++;
+  }
+
+  for(int i = 0; i < 26; i++){
+    char letter = i + 97;
+    float freq = letters[i] / count;
+    printf("%c: %f\n", letter, freq);
+  }
 
   return(0);
-
 }
